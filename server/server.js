912a223044218ae8b96990d6b095969859ba38fa
Naga Kummaragunta <nkummara@oradev.oraclecorp.com>
2018-02-11 19:19:36 -0800

    add server and client handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path = require('path');
 const express = require('express');
+const socketIO= require('socket.io');
+const http = require('http');
+
 const publicPath=path.join(__dirname,"../client");
 const port=process.env.PORT || 3000;
 
 const app = express();
+var server= http.createServer(app);
+var io = socketIO(server);
+io.on('connection',(socket) => {
+  console.log("New user connected");
+
+  socket.on('disconnect',() => {
+    console.log('User was Disconnected');
+  })
+})
 
 app.use(express.static(publicPath));
-app.listen(port,() =>{
+server.listen(port,() =>{
   console.log(`Server is running on port ${port}`);
 });
